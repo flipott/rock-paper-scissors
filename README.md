642d0fe573de4beac3a7c7a59d098cab8af518c5
flipott <ottphillipa@gmail.com>
2022-03-12 17:41:07 -0500

    Designed webpage and refactored JavaScript code to work with HTML.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # rock-paper-scissors
 
 This is a basic rock-paper-scissors game that can be played in the console of the webpage. It demonstrates a foundational knowledge of Javascript programming, featuring functions and conditional statements.
+
+
+# Attribution
+Rock icon created by Freepik on Flaticon: https://www.flaticon.com/free-icon/rock_500351
+Paper icon created by Freepik on Flaticon: https://www.flaticon.com/premium-icon/send_2161491
+Scissors icon created by Freepik on Flaticon: https://www.flaticon.com/free-icon/scissors_2168956
--- game.js
+++ game.js
@@ -1,19 +1,86 @@
 let moves = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll(".btn");
+const playerScoreDisplay = document.querySelector("#player-score-display");
+const computerScoreDisplay = document.querySelector("#computer-score-display");
+const roundResult = document.querySelector("#round-result");
+const winner = document.querySelector("#winner");
+let playerChoice;
+let playerScore = 0;
+let computerScore = 0;
+let currResults = [];
+
+buttons.forEach(button => button.addEventListener('click',()=>{
+    playerChoice = button.id;
+    game()
+}))
+ 
+function game() {
+
+    winner.textContent = "";
+
+    if ((playerScore < 5) && (computerScore < 5)) {
+        currResults = playRound(playerChoice, computerPlay());
+    }; 
+
+    if (currResults[1] > 0) {
+        roundResult.textContent += " You win this round!";
+        playerScore++;
+    } else if (currResults[1] < 0) {
+        roundResult.textContent += " You lose this round...";
+        computerScore++;
+    }
+
+    playerScoreDisplay.style.borderStyle = "solid";
+    playerScoreDisplay.style.fontSize = "22px";
+    playerScoreDisplay.style.fontWeight = "bold";
+    playerScoreDisplay.style.color = "rgba(25, 25, 27, 0.767)"
+    playerScoreDisplay.style.padding = "8px";
+
+    computerScoreDisplay.style.borderStyle = "solid";
+    computerScoreDisplay.style.fontSize = "22px";
+    computerScoreDisplay.style.fontWeight = "bold";
+    computerScoreDisplay.style.color = "rgba(25, 25, 27, 0.767)"
+    computerScoreDisplay.style.padding = "8px";
+
+    playerScoreDisplay.textContent = "Player score: " + playerScore;
+    computerScoreDisplay.textContent = "Computer score: " + computerScore;
+
+    winCheck();
+}
+
+function winCheck() {
+
+    if (playerScore == 5) {
+        winner.textContent = "YOU WON! Make a selection if you'd like to play another game.";
+        winner.style.backgroundColor = "lightgreen";
+        playerScore = 0;
+        computerScore = 0;
+        currResults = [];
+    } else if (computerScore == 5) {
+        winner.textContent = "YOU LOST! Make a selection if you'd like to play another game.";
+        winner.style.backgroundColor = "coral";
+        playerScore = 0;
+        computerScore = 0;
+        currResults = [];
+    }
+
+}
+
+
 
 //This function picks a random move for the CPU. 
 function computerPlay() {
     return moves[Math.floor(Math.random() * 3)]
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt().toLowerCase()
-    computerSelection = computerPlay()
 
-    console.log(playerSelection)
-    console.log(computerSelection)
+function playRound(playerSelection, computerSelection) {
+
+    roundResult.textContent = "You chose " + playerSelection + ". CPU chose " + computerSelection + "."
 
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
+            roundResult.textContent += " Draw!"
             return ["You both played rock. The game is tied!", 0];
         } else if (computerSelection === "scissors") {
             return ["Rock destroys scissors. You win!", 1];
@@ -22,6 +89,7 @@ function playRound(playerSelection, computerSelection) {
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "paper") {
+            roundResult.textContent += " Draw!"
             return ["You both played paper. The game is tied!", 0];
         } else if (computerSelection === "scissors") {
             return ["Scissors cuts paper. You lose!", -1]
@@ -30,6 +98,7 @@ function playRound(playerSelection, computerSelection) {
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "scissors") {
+            roundResult.textContent += " Draw!"
             return ["You both played scissors. The game is tied!", 0];
         } else if (computerSelection === "rock") {
             return ["Rock destroys scissors. You lose!", -1]
@@ -39,105 +108,3 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-function game() {
-
-    let gameCount = 0;
-    let playerScore = 0;
-    let computerScore = 0;
-
-    firstRound = playRound()
-
-    if (firstRound[1] === 0) {
-        gameCount += 1;
-    } else if  (firstRound[1] === 1) {
-        gameCount += 1;
-        playerScore += 1;
-    } else if (firstRound[1] === -1) {
-        gameCount += 1;
-        computerScore += 1;
-    }
-
-    console.log(firstRound[0]);
-    console.log("Games played: " + gameCount);
-    console.log("Player score: " + playerScore);
-    console.log("CPU score: " + computerScore);
-
-    secondRound = playRound()
-
-    if (secondRound[1] === 0) {
-        gameCount += 1;
-    } else if  (secondRound[1] === 1) {
-        gameCount += 1;
-        playerScore += 1;
-    } else if (secondRound[1] === -1) {
-        gameCount += 1;
-        computerScore += 1;
-    }
-
-    console.log(secondRound[0]);
-    console.log("Games played: " + gameCount);
-    console.log("Player score: " + playerScore);
-    console.log("CPU score: " + computerScore);
-
-    thirdRound = playRound()
-
-    if (thirdRound[1] === 0) {
-        gameCount += 1;
-    } else if  (thirdRound[1] === 1) {
-        gameCount += 1;
-        playerScore += 1;
-    } else if (thirdRound[1] === -1) {
-        gameCount += 1;
-        computerScore += 1;
-    }
-
-    console.log(thirdRound[0]);
-    console.log("Games played: " + gameCount);
-    console.log("Player score: " + playerScore);
-    console.log("CPU score: " + computerScore);
-
-    fourthRound = playRound()
-
-    if (fourthRound[1] === 0) {
-        gameCount += 1;
-    } else if  (fourthRound[1] === 1) {
-        gameCount += 1;
-        playerScore += 1;
-    } else if (fourthRound[1] === -1) {
-        gameCount += 1;
-        computerScore += 1;
-    }
-
-    console.log(fourthRound[0]);
-    console.log("Games played: " + gameCount);
-    console.log("Player score: " + playerScore);
-    console.log("CPU score: " + computerScore);
-
-    fifthRound = playRound()
-
-    if (fifthRound[1] === 0) {
-        gameCount += 1;
-    } else if  (fifthRound[1] === 1) {
-        gameCount += 1;
-        playerScore += 1;
-    } else if (fifthRound[1] === -1) {
-        gameCount += 1;
-        computerScore += 1;
-    }
-
-    console.log(fifthRound[0]);
-    console.log("Games played: " + gameCount);
-    console.log("Player score: " + playerScore);
-    console.log("CPU score: " + computerScore);
-
-    if (playerScore > computerScore) {
-        return "You win it all! Refresh to play again."
-    } else if (playerScore < computerScore) {
-        return "You lost it all! Refresh to play again."
-    } else if (playerScore === computerScore) {
-        return "You and the CPU tied! Refresh to play again."
-    }
-}
-
-console.log(game())
